892e41f69720851d0e3b2494e6ca9a0fee7e0238
alisher <a.sapalov@mail.ru>
2021-02-25 19:42:56 +0600

    second commit

--- wall/models.py
+++ wall/models.py
@@ -6,6 +6,9 @@ class Wall(models.Model):
     description = models.CharField(max_length=150)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
+    def __str__(self):
+        return self.description
+
 
 class Post(models.Model):
     message = models.TextField()
@@ -13,7 +16,10 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='posts', null=True)
+    image = models.ImageField(upload_to='posts', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.message}, creator: {self.created_by}"
 
 
 class Comment(models.Model):
@@ -23,7 +29,16 @@ class Comment(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return f"{self.message}, commentator: {self.created_by}"
+
+
 
 class Following(models.Model):
     target = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
-    follower =models.ForeignKey(User, on_delete=models.CASCADE, related_name='targets')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='targets')
+    followers_list = []
+    followers_list.append(follower)
+
+    def __str__(self):
+        return f"{self.target} is followed by {self.followers_list}"
